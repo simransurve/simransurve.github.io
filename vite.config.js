21697373c06c949ee68840094d7597f41d917081
ShubhamSurve21 <shubhams2121@gmail.com>
2026-04-08 09:52:16 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/simransurve.github.io/',
+  plugins: [react()],
 })
