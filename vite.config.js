58aac854835facbc4556bc1a198d11111d1c8ac5
ShubhamSurve21 <shubhams2121@gmail.com>
2026-04-08 09:45:48 +0530

    Fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
-  }
 })
